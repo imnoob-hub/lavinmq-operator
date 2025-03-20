5a4edeb040a5b901a42a74c6891b7eec99c9f210
Anton Dalgren <anton@84codes.com>
2025-03-20 09:57:35 +0100

    WIP: init

---
+++ api/v1alpha1/lavinmq_types.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// LavinMQSpec defines the desired state of LavinMQ
+type LavinMQSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Image    string `json:"image,omitempty"`
+	Replicas int32  `json:"replicas,omitempty"`
+
+	// +required
+	Ports []corev1.ContainerPort `json:"ports"`
+	// +required
+	DataVolumeClaimSpec corev1.PersistentVolumeClaimSpec `json:"dataVolumeClaim"`
+}
+
+// LavinMQStatus defines the observed state of LavinMQ
+type LavinMQStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions store the status conditions of the LavinMQ instances
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// LavinMQ is the Schema for the lavinmqs API
+type LavinMQ struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LavinMQSpec   `json:"spec,omitempty"`
+	Status LavinMQStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// LavinMQList contains a list of LavinMQ
+type LavinMQList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LavinMQ `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LavinMQ{}, &LavinMQList{})
+}
