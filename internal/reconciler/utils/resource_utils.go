b543f2007366ea7c3fbe22939baa20165a44c109
Oskar Westberg <oskar.isidor.gustafsson@gmail.com>
2025-05-06 15:10:08 +0200

    Fix: != operator do not work for Type resource.Quantity, length is also not working, as it may contain a ephermal storage to

--- internal/reconciler/utils/resource_utils.go
+++ internal/reconciler/utils/resource_utils.go
@@ -4,30 +4,19 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// equalResourceRequirements compares two ResourceRequirements objects using the Cmp method for each resource
 func EqualResourceRequirements(a, b corev1.ResourceRequirements) bool {
-	// Compare Requests
 	if !equalResourceLists(a.Requests, b.Requests) {
 		return false
 	}
-
-	// Compare Limits
-	if !equalResourceLists(a.Limits, b.Limits) {
-		return false
-	}
-
-	return true
+	return equalResourceLists(a.Limits, b.Limits)
 }
 
 func equalResourceLists(a, b corev1.ResourceList) bool {
-	if len(a) != len(b) {
+	if a.Cpu().Cmp(*b.Cpu()) != 0 {
 		return false
 	}
-	for k, v := range a {
-		if bv, exists := b[k]; !exists || v.Cmp(bv) != 0 {
-			return false
-		}
+	if a.Memory().Cmp(*b.Memory()) != 0 {
+		return false
 	}
-
 	return true
 }
